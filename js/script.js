dee694a2e96639f9ceb944707a633428f1becd47
Alexey <preobrazhenskyalexsey@gmail.com>
2023-07-27 00:18:38 +0300

    tabs are working

---
+++ js/script.js
@@ -0,0 +1,37 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+	const tabs = document.querySelectorAll(".tabheader__item");
+	const tabheaderItems = document.querySelector(".tabheader__items");
+	const tabsContent = document.querySelectorAll(".tabcontent");
+
+	function hideTabContent() {
+		tabs.forEach((item) => {
+			item.classList.remove("tabheader__item_active");
+		});
+		tabsContent.forEach((item) => {
+			item.classList.add("hide");
+			item.classList.remove("show", "fade");
+		});
+	}
+
+	function showTabContent(i = 0) {
+		tabs[i].classList.add("tabheader__item_active");
+		tabsContent[i].classList.add("show", "fade");
+		tabsContent[i].classList.remove("hide");
+	}
+
+	hideTabContent();
+	showTabContent();
+
+	tabheaderItems.addEventListener("click", event => {
+		const target = event.target;
+		if (target && target.classList.contains("tabheader__item")) {
+			tabs.forEach((item, i) => {
+				if (item == target) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+  
+});
