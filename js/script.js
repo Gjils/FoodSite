c349f61cb66e1bc4eff421a589949a203eccd260
Alexey <preobrazhenskyalexsey@gmail.com>
2023-07-27 01:10:18 +0300

    timer are working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
+	// Tabs
+
 	const tabs = document.querySelectorAll(".tabheader__item");
 	const tabheaderItems = document.querySelector(".tabheader__items");
 	const tabsContent = document.querySelectorAll(".tabcontent");
@@ -33,5 +35,51 @@
 			});
 		}
 	});
+
+	// Timer
+
+	const deadline = "2024-05-20";
+
+	function getTimeRemaining(endtime) {
+		const total = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor(total / (1000 * 60 * 60 * 24)),
+			hours = Math.floor(total / (1000 * 60 * 60) % 24),
+			minutes = Math.floor(total / (1000 * 60) % 60),
+			seconds = Math.floor(total / 1000 % 60);
+
+		return {total, days, hours, minutes, seconds};
+	}
+
+	function getZero(number) {
+		if (number <= 9) {
+			return "0" + number;
+		}
+		return number;
+	}
+
+	function setClock(selector, deadline) {
+		const timer = document.querySelector(selector),
+			days = timer.querySelector("#days"),
+			hours = timer.querySelector("#hours"),
+			minutes = timer.querySelector("#minutes"),
+			seconds = timer.querySelector("#seconds"),
+			timeInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+
+		function updateClock() {
+			const timeRemaining = getTimeRemaining(deadline);
+			days.innerHTML = getZero(timeRemaining.days);
+			hours.innerHTML = getZero(timeRemaining.hours);
+			minutes.innerHTML = getZero(timeRemaining.minutes);
+			seconds.innerHTML = getZero(timeRemaining.seconds);
+
+			if (timeRemaining.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	setClock(".timer", deadline);
   
 });
