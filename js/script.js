c18e8abc29a07fc0343acc9b0e8b792fc3d4bfc7
Alexey <preobrazhenskyalexey@gmail.com>
2023-07-29 21:33:03 +0300

    Sending forms with XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -189,4 +189,59 @@
 	отсутствие продуктов животного происхождения, молоко из миндаля,
 	овса, кокоса или гречки, правильное количество белков за счет тофу
 	и импортных вегетарианских стейков.`, 430, ".menu__field .container").render();
+
+	// Forms
+
+	const forms = document.querySelectorAll("form");
+
+	const message = {
+		loading: "Загрузка...",
+		success: "Спасибо, скоро мы с вами свяжемся",
+		failure: "Что-то пошло не так"
+	};
+
+	function postData(form) {
+		form.addEventListener("submit", (event) => {
+			event.preventDefault();
+
+			const statusMessage = document.createElement("div");
+			statusMessage.classList.add("status");
+			statusMessage.textContent = message.loading;
+			form.append(statusMessage);
+
+			const request = new XMLHttpRequest();
+			request.open("POST", "server.php");
+
+			request.setRequestHeader("Content-type", "application/json");
+			const formData = new FormData(form);
+
+			const object = {};
+			console.log(formData);
+			formData.forEach((value, key) => {
+				object[key] = value;
+			});
+
+			const json = JSON.stringify(object);
+			
+			request.send(json);
+
+			request.addEventListener("load", () => {
+				if (request.status == 200) {
+					console.log(request.response);
+					statusMessage.textContent = message.success;
+					form.reset();
+					setTimeout(() => {
+						statusMessage.remove();
+					}, 2000);
+				} else {
+					console.log(request.response);
+					statusMessage.textContent = message.failure;
+				}
+			});
+		});
+	}
+
+	forms.forEach(item => {
+		postData(item);
+	});
 });
